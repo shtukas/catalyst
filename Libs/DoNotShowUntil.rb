5f8d1641d6979f61f992c3c01c9cc59003af217a
Pascal Honoré <pascal@alseyn.net>
2023-03-26 16:33:58 +0100

    refactoring

--- Libs/DoNotShowUntil.rb
+++ Libs/DoNotShowUntil.rb
@@ -4,20 +4,27 @@ class DoNotShowUntil
     # DoNotShowUntil::setUnixtime(item, unixtime)
     def self.setUnixtime(item, unixtime)
         # We start by using XCache for the special purpose of backup items on alexandra
-        XCache::set(item["uuid"], unixtime)
+        XCache::set("DoNotShowUntil:#{item["uuid"]}", unixtime)
 
         item["doNotShowUntil"] = unixtime
         N3Objects::commit(item)
     end
 
+    # DoNotShowUntil::getUnixtimeOrNull(item)
+    def self.getUnixtimeOrNull(item)
+        return item["doNotShowUntil"] if item["doNotShowUntil"]
+        return XCache::getOrNull("DoNotShowUntil:#{item["uuid"]}").to_f if XCache::getOrNull("DoNotShowUntil:#{item["uuid"]}")
+        nil
+    end
+
     # DoNotShowUntil::isVisible(item)
     def self.isVisible(item)
-        Time.new.to_i >= (item["doNotShowUntil"] || 0)
+        Time.new.to_i >= (DoNotShowUntil::getUnixtimeOrNull(item) || 0)
     end
 
     # DoNotShowUntil::suffixString(item)
     def self.suffixString(item)
-        unixtime = (item["doNotShowUntil"] || (XCache::getOrNull(item["uuid"]) || 0)).to_f
+        unixtime = (DoNotShowUntil::getUnixtimeOrNull(item) || 0)
         return "" if unixtime.nil?
         return "" if Time.new.to_i > unixtime
         " (not shown until: #{Time.at(unixtime).to_s})"
