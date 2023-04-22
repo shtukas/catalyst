63296b885d73727e70eb4b8e21fe6dcc9e2098ce
Pascal Honoré <pascal@alseyn.net>
2023-04-22 19:13:03 +0100

    Listing

--- Libs/Listing.rb
+++ Libs/Listing.rb
@@ -844,7 +844,7 @@ class Listing
         if item["mikuType"] == "NxTask" then
             return true if NxTasks::completionRatio(item) >= 1
         end
-        return true if item["mikuType"] == "Float"
+        return true if item["mikuType"] == "NxFloat"
         return true if !DoNotShowUntil::isVisible(item)
         false
     end
