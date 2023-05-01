3878fcc31089ea6dc7dc7e353de40b44143d71a4
Pascal Honoré <pascal@alseyn.net>
2023-05-01 11:52:28 +0100

    correction

--- Libs/NxFrontOrdinals.rb
+++ Libs/NxFrontOrdinals.rb
@@ -74,35 +74,34 @@ class NxFrontOrdinals
     # NxFrontOrdinals::dataManagement(queue2)
     def self.dataManagement(queue2)
 
-        # Nothing to do if the queue is empty
-        return if NxFrontOrdinals::items().empty?
-
-        # Prevent the ordinal values to grow beyond 100 (for 2 digits main value)
-        maxordinal = NxFrontOrdinals::items().map{|item| item["targetordinal"] }.max
-        return if maxordinal < 100
-        NxFrontOrdinals::items().map{|item|
-            item["targetordinal"] = item["targetordinal"] * 0.5
-            N3Objects::commit(item)
-        }
-
         # if we have less than 10 items in the fifo, we add the first from queue2
-        if NxFrontOrdinals::items().size < 10 then
+        if NxFrontOrdinals::items().size < 10 and !queue2.empty? then
             NxFrontOrdinals::addAtTheEnd(queue2.first)
             return
         end
 
-        # Get the last items of fifo
-        fo = NxFrontOrdinals::items().last
-        item = N3Objects::getOrNull(fo["uuid"])
-        if item.nil? then
-            N3Objects::destroy(fo["uuid"])
+        # Prevent the ordinal values to grow beyond 100 (for 2 digits main value)
+        if NxFrontOrdinals::items().map{|item| item["targetordinal"] }.max > 99 then
+            NxFrontOrdinals::items().map{|item|
+                item["targetordinal"] = item["targetordinal"] * 0.5
+                N3Objects::commit(item)
+            }
             return
         end
 
-        # If the last items is a wave that is not an interruption, then we pick a random non wave, if there is one
-        if item["mikuType"] == "Wave" and !item["interruption"] and (picks = queue2.select{|item| item["mikuType"] != "Wave" }).size > 0 then
-            NxFrontOrdinals::addAtTheEnd(picks.first)
-            return
+        # Get the last items of fifo
+        if NxFrontOrdinals::items().size > 0 then
+            fo = NxFrontOrdinals::items().last
+            item = N3Objects::getOrNull(fo["uuid"])
+            if item then
+                if item["mikuType"] == "Wave" and !item["interruption"] and (picks = queue2.select{|item| item["mikuType"] != "Wave" }).size > 0 then
+                    NxFrontOrdinals::addAtTheEnd(picks.first)
+                    return
+                end
+            else
+                N3Objects::destroy(fo["uuid"])
+                return
+            end
         end
     end
 
