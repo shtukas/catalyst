e0a4d8aba532c86b0b858f6f4f9995b5b68e4eff
Pascal Honoré <pascal@alseyn.net>
2023-06-03 20:32:25 +0100

    correction

--- Libs/CatalystSearch.rb
+++ Libs/CatalystSearch.rb
@@ -7,7 +7,7 @@ class CatalystSearch
             fragment = LucilleCore::askQuestionAnswerAsString("search fragment (empty to abort) : ")
             break if fragment == ""
             selected = Catalyst::catalystItems()
-                            .select{|item| item["description"].downcase.include?(fragment.downcase) }
+                            .select{|item| item["description"] and item["description"].downcase.include?(fragment.downcase) }
             if selected.empty? then
                 puts "Could not find a matching element for '#{fragment}'"
                 LucilleCore::pressEnterToContinue()
@@ -16,7 +16,7 @@ class CatalystSearch
             loop {
                 system('clear')
                 selected = Catalyst::catalystItems()
-                                .select{|item| item["description"].downcase.include?(fragment.downcase) }
+                                .select{|item| item["description"] and item["description"].downcase.include?(fragment.downcase) }
                                 .sort{|i1, i2| i1["unixtime"] <=> i2["unixtime"] }
                 item = LucilleCore::selectEntityFromListOfEntitiesOrNull("item", selected, lambda{|i| i["description"] })
                 break if item.nil?
