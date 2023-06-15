27ad31183ee19c815c1cc5afc5e9b5bbbdb752eb
Pascal Honoré <pascal@alseyn.net>
2023-06-15 23:16:31 +0100

    corrections

--- Libs/NxBackups.rb
+++ Libs/NxBackups.rb
@@ -69,7 +69,7 @@ class NxBackups
 
     # NxBackups::toString(item)
     def self.toString(item)
-        "💿 #{item["description"]}"
+        "💾 #{item["description"]}"
     end
 
     # NxBackups::listingItems()
