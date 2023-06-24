7d89bf6cbc6c9dc1e687d5342f9b7a08394d3526
Pascal Honoré <pascal@alseyn.net>
2023-06-24 12:18:16 +0100

    upgrade

--- Libs/Listing.rb
+++ Libs/Listing.rb
@@ -298,6 +298,7 @@ class Listing
             end
 
             if ProgrammableBooleans::trueNoMoreOftenThanEveryNSeconds("fd3b5554-84f4-40c2-9c89-1c3cb2a67717", 3600) then
+                puts "Listing::maintenance()"
                 Listing::maintenance()
             end
 
