f66a51ee76ff01b87191c3f3f88cfde14c3ef622
Pascal Honoré <pascal@alseyn.net>
2023-07-01 07:46:30 +0100

    anti-matter corrections

--- Libs/DxAntimatter.rb
+++ Libs/DxAntimatter.rb
@@ -1,20 +1,21 @@
 
 class DxAntimatters
 
-    # DxAntimatters::issue(familyId, description, charge)
-    def self.issue(familyId, description, charge)
+    # DxAntimatters::issue(familyId, description, initialValue)
+    def self.issue(familyId, description, initialValue)
         uuid = SecureRandom.uuid
         DarkEnergy::init("DxAntimatter", uuid)
         DarkEnergy::patch(uuid, "unixtime", Time.new.to_i)
         DarkEnergy::patch(uuid, "datetime", Time.new.utc.iso8601)
         DarkEnergy::patch(uuid, "description", description)
         DarkEnergy::patch(uuid, "familyId", familyId)
-        DarkEnergy::patch(uuid, "charge", charge)
+        DarkEnergy::patch(uuid, "initialValue", initialValue)
         DarkEnergy::itemOrNull(uuid)
     end
 
     # DxAntimatters::toString(item)
     def self.toString(item)
-        "🍄 #{item["description"]} #{(item["charge"].to_f/3600).round(2)} hours"
+       valueInHours = (item["initialValue"] + Bank::getValue(item["uuid"])).to_f/3600
+        "🍄 #{item["description"]} #{valueInHours.round(2)} hours"
     end
 end
