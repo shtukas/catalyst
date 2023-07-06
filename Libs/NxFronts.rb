a0f4142bba755f042b1386c33aa8f9102dc3b5ed
Pascal Honoré <pascal@alseyn.net>
2023-07-06 06:31:02 +0100

    correct NxFront maintenance

--- Libs/NxFronts.rb
+++ Libs/NxFronts.rb
@@ -17,7 +17,14 @@ class NxFronts
     def self.interactivelyIssueNewOrNull()
         description = LucilleCore::askQuestionAnswerAsString("description (empty to abort): ")
         return nil if description == ""
-        NxFronts::issueWithDescription(description)
+        uuid = SecureRandom.uuid
+        DarkEnergy::init("NxFront", uuid)
+        coredataref = CoreData::interactivelyMakeNewReferenceStringOrNull()
+        DarkEnergy::patch(uuid, "unixtime", Time.new.to_i)
+        DarkEnergy::patch(uuid, "datetime", Time.new.utc.iso8601)
+        DarkEnergy::patch(uuid, "description", description)
+        DarkEnergy::patch(uuid, "field11", coredataref)
+        DarkEnergy::itemOrNull(uuid)
     end
 
     # NxFronts::toString(item)
@@ -34,8 +41,6 @@ class NxFronts
             DarkEnergy::patch(uuid, "datetime", Time.new.utc.iso8601)
             DarkEnergy::patch(uuid, "description", Time.new.to_s)
             item = DarkEnergy::itemOrNull(uuid)
-
-            item = NxFronts::issueWithDescription(Time.new.to_s)
             ListingPositions::set(item, ListingPositions::nextPosition())
             XCache::setFlag("eea69539-f90d-4400-9d05-d806f97784a6:#{CommonUtils::today()}", true)
         end
