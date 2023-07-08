0e374cd645ac7d2921c8d05a53fad09532d5b74e
Pascal Honoré <pascal@alseyn.net>
2023-07-08 10:30:29 +0100

    Dx update

--- Libs/DxAntimatter.rb
+++ Libs/DxAntimatter.rb
@@ -21,7 +21,7 @@ class DxAntimatters
     def self.toString(item)
         target = DarkEnergy::itemOrNull(item["targetuuid"])
         valueInHours = DxAntimatters::value(item).to_f/3600
-        "🍄 #{target ? "(target:) #{target["description"]}" : "(no target found)"} (needs: #{-valueInHours.round(2)} hours)"
+        "🍄 #{target ? "(target:) #{PolyFunctions::toString(target)}" : "(no target found)"} (needs: #{-valueInHours.round(2)} hours)"
     end
 
     # DxAntimatters::listingItems()
