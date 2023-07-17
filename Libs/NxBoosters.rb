9908db9f21505992e905d6952063d116877a0918
Pascal Honoré <pascal@alseyn.net>
2023-07-17 18:27:26 +0100

    -

--- Libs/NxBoosters.rb
+++ Libs/NxBoosters.rb
@@ -12,7 +12,7 @@ class NxBoosters
 
     # NxBoosters::toString(item)
     def self.toString(item)
-        "🚀 (#{"%6.2f" % (100*NxBoosters::ratio(item["uuid"]))} % of #{"%4.2f" % item["hours"]} hours) #{item["description"]}#{TxCores::suffix(item)}"
+        "🚀 (#{"%6.2f" % (100*NxBoosters::ratio(item["uuid"]))} % of #{"%4.2f" % item["hours"]} hours) #{item["description"]}"
     end
 
     # NxBoosters::ratio(uuid)
@@ -29,13 +29,13 @@ class NxBoosters
             .map{|uuid|
                 description = Blades::getAttributeOrNull2(uuid, "description")
                 hours = Blades::getAttributeOrNull2(uuid, "hours")
-                parent = Blades::getAttributeOrNull2(uuid, "parent")
+                core = Blades::getAttributeOrNull2(uuid, "core")
                 {
                     "uuid"        => uuid,
                     "mikuType"    => "NxBooster",
                     "hours"       => hours,
                     "description" => description,
-                    "parent"      => parent
+                    "core"        => core
                 }
             }
     end
