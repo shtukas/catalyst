6316ac7a10d77d2433aa82ed6bbb6f56bd54e89c
Pascal Honoré <pascal@alseyn.net>
2023-07-18 09:49:37 +0100

    NxPromises updates

--- Libs/NxPromises.rb
+++ Libs/NxPromises.rb
@@ -31,29 +31,33 @@ class NxPromises
     # ---------------------------------------------------
     # Data
 
+    # NxPromises::uuidToItem(uuid)
+    def self.uuidToItem(uuid)
+        description   = Blades::getAttributeOrNull2(uuid, "description")
+        datetimeStart = Blades::getAttributeOrNull2(uuid, "datetimeStart")
+        datetimeEnd   = Blades::getAttributeOrNull2(uuid, "datetimeEnd")
+        loadInHours   = Blades::getAttributeOrNull2(uuid, "loadInHours")
+        {
+            "uuid"          => uuid,
+            "mikuType"      => "NxPromise",
+            "description"   => description,
+            "datetimeStart" => datetimeStart,
+            "datetimeEnd"   => datetimeEnd,
+            "loadInHours"   => loadInHours
+        }
+    end
+
     # NxPromises::items()
     def self.items()
         Solingen::mikuTypeUUIDs("NxPromise")
-            .map{|uuid|
-                description   = Blades::getAttributeOrNull2(uuid, "description")
-                datetimeStart = Blades::getAttributeOrNull2(uuid, "datetimeStart")
-                datetimeEnd   = Blades::getAttributeOrNull2(uuid, "datetimeEnd")
-                loadInHours   = Blades::getAttributeOrNull2(uuid, "loadInHours")
-                {
-                    "uuid"          => uuid,
-                    "mikuType"      => "NxPromise",
-                    "description"   => description,
-                    "datetimeStart" => datetimeStart,
-                    "datetimeEnd"   => datetimeEnd,
-                    "loadInHours"   => loadInHours
-                }
-            }
+            .map{|uuid| NxPromises::uuidToItem(uuid) }
     end
 
     # NxPromises::toString(item)
     def self.toString(item)
         cr = NxPromises::completionRatio(item)
-        "🔅 (rc: #{"%6.2f" % (100*cr)} %) #{item["description"]}"
+        lr = NxPromises::loadIndex(item)
+        "🔅 (cr: #{"%6.2f" % (100*cr)} %) (li: #{"%5.3f" % lr}) #{item["description"]}"
     end
 
     # NxPromises::listingItems()
@@ -65,6 +69,13 @@ class NxPromises
     def self.completionRatio(item)
         doneInHours = Bank::getValue(item["uuid"]).to_f/3600
         doneInHours.to_f/item["loadInHours"]
+    end
+
+    # NxPromises::loadIndex(item)
+    def self.loadIndex(item)
+        hoursLeftToDo = item["loadInHours"] - Bank::getValue(item["uuid"]).to_f/3600
+        hoursToDeadline = (DateTime.parse(item["datetimeEnd"]).to_time.to_i - Time.new.to_i).to_f/3600
+        hoursLeftToDo.to_f/hoursToDeadline
     end
 
     # ---------------------------------------------------
