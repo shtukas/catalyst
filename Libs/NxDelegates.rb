59af3718ec9111e7c8cf93506cb4c3874bb20acd
Pascal Honoré <pascal@alseyn.net>
2023-07-20 16:06:38 +0100

    refactoring

--- Libs/NxDelegates.rb
+++ Libs/NxDelegates.rb
@@ -21,17 +21,21 @@ class NxDelegates
 
     # NxDelegates::interactivelyIssueNewAtParentOrNull(parent)
     def self.interactivelyIssueNewAtParentOrNull(parent)
+        position = nil
         if parent["mikuType"] == "NxThread" then
             position = 0
-        else
+        end
+        if parent["mikuType"] == "NxCore" then
+            position = 0
+        end
+        if position.nil? then
             position = Tx8s::interactivelyDecidePositionUnderThisParent(parent)
         end
         tx8 = Tx8s::make(parent["uuid"], position)
+        delegate = NxDelegates::interactivelyIssueNewOrNull()
+        return nil if delegate.nil?
 
-        float = NxDelegates::interactivelyIssueNewOrNull()
-        return nil if float.nil?
-
-        DarkEnergy::patch(float["uuid"], "parent", tx8)
+        DarkEnergy::patch(delegate["uuid"], "parent", tx8)
     end
 
     # NxDelegates::toString(item)
@@ -42,22 +46,22 @@ class NxDelegates
     # NxDelegates::listingItemsForMainListing()
     def self.listingItemsForMainListing()
         DarkEnergy::mikuType("NxDelegate")
-            .select{|float| float["parent"].nil? }
+            .select{|delegate| delegate["parent"].nil? }
     end
 
     # NxDelegates::listingItemsForThread(thread)
     def self.listingItemsForThread(thread)
         DarkEnergy::mikuType("NxDelegate")
-            .select{|float| float["parent"] and float["parent"]["uuid"] == thread["uuid"] }
+            .select{|delegate| delegate["parent"] and delegate["parent"]["uuid"] == thread["uuid"] }
     end
 
     # NxDelegates::maintenance()
     def self.maintenance()
         DarkEnergy::mikuType("NxDelegate")
-            .each{|float| 
-                next if float["parent"].nil?
-                if DarkEnergy::itemOrNull(float["parent"]["uuid"]).nil? then
-                    DarkEnergy::patch(float["uuid"], "parent", nil)
+            .each{|delegate| 
+                next if delegate["parent"].nil?
+                if DarkEnergy::itemOrNull(delegate["parent"]["uuid"]).nil? then
+                    DarkEnergy::patch(delegate["uuid"], "parent", nil)
                 end
             }
     end
@@ -65,9 +69,9 @@ class NxDelegates
     # NxDelegates::program1()
     def self.program1()
         loop {
-            float = LucilleCore::selectEntityFromListOfEntitiesOrNull("float", DarkEnergy::mikuType("NxDelegate"), lambda{|float| NxDelegates::toString(float) })
-            return if float.nil?
-            PolyActions::access(float)
+            delegate = LucilleCore::selectEntityFromListOfEntitiesOrNull("delegate", DarkEnergy::mikuType("NxDelegate"), lambda{|delegate| NxDelegates::toString(delegate) })
+            return if delegate.nil?
+            PolyActions::access(delegate)
         }
     end
 end
