61bfe50f0f100f4595f9ce647b780b121f5333e8
Pascal Honoré <pascal@alseyn.net>
2023-07-31 22:04:48 +0100

    -

--- Libs/Pure.rb
+++ Libs/Pure.rb
@@ -1,7 +1,7 @@
 class Pure
 
-    # Pure::containerChildrenInOrder(container)
-    def self.containerChildrenInOrder(container)
+    # Pure::childrenInOrder3(container)
+    def self.childrenInOrder3(container)
         if container["mikuType"] == "TxCore" and container["uuid"] == "77a43c09-4642-45ff-b174-09898175919a" then
             # L & P to P
             items  = Tx8s::childrenInOrder(container)
@@ -37,25 +37,27 @@ class Pure
         ].sort_by{|packet| packet["rt"] }.map{|packet| packet["items"] }.flatten
     end
 
-    # Pure::childrenInOrder(item)
-    def self.childrenInOrder(item)
+    # Pure::childrenInOrder2(item)
+    def self.childrenInOrder2(item)
         if item["mikuType"] == "NxThread" then
-            return Pure::containerChildrenInOrder(item)
+            return Pure::childrenInOrder3(item)
         end
         if item["mikuType"] == "TxCore" then
-            return Pure::containerChildrenInOrder(item)
+            return Pure::childrenInOrder3(item)
         end
         Tx8s::childrenInOrder(item)
     end
 
     # Pure::energy(item)
     def self.energy(item) # prefix + [item]
-        if item["mikuType"] == "NxThread" then
-            return Pure::childrenInOrder(item).take(5) + [item]
+        children = Pure::childrenInOrder2(item)
+        if children.empty? then
+            return [item]
+        else
+            c1 = children.first
+            c2 = children.drop(1).take(5)
+            return Pure::energy(c1) + c2 + [item]
+
         end
-        if item["mikuType"] == "TxCore" then
-            return Pure::childrenInOrder(item).take(5) + [item]
-        end
-        [item]
     end
 end
