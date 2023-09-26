39ea324895ab728ed0d0830b7997b93df05dc322
Pascal Honoré <pascal@alseyn.net>
2023-09-26 20:05:43 +0100

    upgrade

--- Libs/Catalyst.rb
+++ Libs/Catalyst.rb
@@ -171,7 +171,7 @@ class Catalyst
     # Catalyst::children(parent)
     def self.children(parent)
         items = (Catalyst::mikuType("NxThread") + Catalyst::mikuType("NxTask") + Catalyst::mikuType("NxCruise"))
-                    .select{|item| item["coreX-2300"] == parent["uuid"] }
+                    .select{|item| item["coreX-2300"] == parent["uuid"] or item["lineage-nx128"] == parent["uuid"] }
         is1, is2 = items.partition{|thread| thread["engine-0852"] }
         [
             is1.select{|thread| TxEngine::ratio(thread["engine-0852"]) > 0 }.sort_by{|thread| TxEngine::ratio(thread["engine-0852"]) },
