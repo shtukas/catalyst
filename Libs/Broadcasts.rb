32d4c6e1f89b258b1fe1f8bbdc1045d4d8bd53ee
Pascal Honoré <pascal@alseyn.net>
2023-10-07 10:36:02 +0100

    database updates

--- Libs/Broadcasts.rb
+++ Libs/Broadcasts.rb
@@ -102,20 +102,62 @@ class Broadcasts
     # Broadcasts::publishItemAttributeUpdate(itemuuid, attname, attvalue)
     def self.publishItemAttributeUpdate(itemuuid, attname, attvalue)
         Broadcasts::publish(Broadcasts::makeItemAttributeUpdate(itemuuid, attname, attvalue))
+
+        item = Catalyst::itemOrNull(itemuuid)
+        if item then
+            filepath = "#{Config::userHomeDirectory()}/Galaxy/DataHub/catalyst/Instance-Data-Directories/Lucille23/databases/Items.sqlite3"
+            db = SQLite3::Database.new(filepath)
+            db.busy_timeout = 117
+            db.busy_handler { |count| true }
+            db.results_as_hash = true
+            db.execute "delete from Items where _uuid_=?", [itemuuid]
+            db.execute "insert into Items (_uuid_, _mikuType_, _item_) values (?, ?, ?)", [item["uuid"], item["mikuType"], JSON.generate(item)]
+            db.close
+        end
     end
 
     # Broadcasts::publishItemDestroy(itemuuid)
     def self.publishItemDestroy(itemuuid)
         Broadcasts::publish(Broadcasts::makeItemDestroy(itemuuid))
+
+        filepath = "#{Config::userHomeDirectory()}/Galaxy/DataHub/catalyst/Instance-Data-Directories/Lucille23/databases/Items.sqlite3"
+        db = SQLite3::Database.new(filepath)
+        db.busy_timeout = 117
+        db.busy_handler { |count| true }
+        db.results_as_hash = true
+        db.execute "delete from Items where _uuid_=?", [itemuuid]
+        db.close
     end
 
     # Broadcasts::publishItemInit(uuid, mikuType)
     def self.publishItemInit(uuid, mikuType)
         Broadcasts::publish(Broadcasts::makeItemInit(uuid, mikuType))
+
+        item = {
+            "uuid"     => uuid,
+            "mikuType" => mikuType
+        }
+
+        filepath = "#{Config::userHomeDirectory()}/Galaxy/DataHub/catalyst/Instance-Data-Directories/Lucille23/databases/Items.sqlite3"
+        db = SQLite3::Database.new(filepath)
+        db.busy_timeout = 117
+        db.busy_handler { |count| true }
+        db.results_as_hash = true
+        db.execute "insert into Items (_uuid_, _mikuType_, _item_) values (?, ?, ?)", [item["uuid"], item["mikuType"], JSON.generate(item)]
+        db.close
+
     end
 
     # Broadcasts::publishBankDeposit(uuid, date, value)
     def self.publishBankDeposit(uuid, date, value)
         Broadcasts::publish(Broadcasts::makeBankDeposit(uuid, date, value))
+
+        filepath = "#{Config::userHomeDirectory()}/Galaxy/DataHub/catalyst/Instance-Data-Directories/Lucille23/databases/Bank.sqlite3"
+        db = SQLite3::Database.new(filepath)
+        db.busy_timeout = 117
+        db.busy_handler { |count| true }
+        db.results_as_hash = true
+        db.execute "insert into Bank (_recorduuid_, _id_, _date_, _value_) values (?, ?, ?, ?)", [SecureRandom.uuid, uuid, date, value]
+        db.close
     end
 end
