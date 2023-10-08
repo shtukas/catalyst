e1222a8457a1356bbd7434a4ee735d14335b5607
Pascal Honoré <pascal@alseyn.net>
2023-10-08 14:42:45 +0100

    cleaning

--- Libs/Config.rb
+++ Libs/Config.rb
@@ -39,16 +39,6 @@ class Config
         config[key]
     end
 
-    # Config::getOrFail(key)
-    def self.getOrFail(key)
-        config = JSON.parse(IO.read(Config::configFilepath()))
-        value = config[key]
-        if value.nil? then
-            raise "could not extract config key: #{key}"
-        end
-        value
-    end
-
     # Config::set(key, value)
     def self.set(key, value)
         config = JSON.parse(IO.read(Config::configFilepath()))
@@ -58,7 +48,11 @@ class Config
 
     # Config::thisInstanceId()
     def self.thisInstanceId()
-        Config::getOrFail("instanceId")
+        object = JSON.parse(IO.read("#{Config::userHomeDirectory()}/Galaxy/DataBank/Stargate-Config.json"))
+        if object["instanceId"].nil? then
+            raise "(error e6d6caec-397f-48d2-9e6d-60d4b8716eb5)"
+        end
+        object["instanceId"]
     end
 
     # Config::isPrimaryInstance()
