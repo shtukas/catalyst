8312ff4283c7494fa927c64a07c2db99ecd3c97a
Pascal Honoré <pascal@alseyn.net>
2024-01-03 23:44:40 +0000

    listing update

--- Libs/Engined.rb
+++ Libs/Engined.rb
@@ -5,8 +5,13 @@ class Engined
     # Engined::listingItems()
     def self.listingItems()
         tasks = Cubes2::mikuType("NxTask").select{|item| item["engine-0020"] }
-        (tasks + NxBlocks::topBlocks())
-            .select{|item| NxBlocks::dayCompletionRatio(item) < 1 }
-            .sort_by{|item| NxBlocks::dayCompletionRatio(item) }
+        items = (tasks + NxBlocks::topBlocks())
+        p1, p2 = items.partition{|item| item["engine-0020"]["type"] == "daily-hours" }
+        [p1, p2].map{|px|
+            px
+                .select{|item| NxBlocks::dayCompletionRatio(item) < 1 }
+                .sort_by{|item| NxBlocks::dayCompletionRatio(item) }
+        }
+        .flatten
     end
 end
