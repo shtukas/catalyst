47f561b55122510a36a6af625d82709bcb20b304
Pascal Honoré <pascal@alseyn.net>
2024-01-09 15:29:17 +0000

    Engined listing update

--- Libs/Engined.rb
+++ Libs/Engined.rb
@@ -1,16 +1,20 @@
 
-
 class Engined
 
     # Engined::muiItems()
     def self.muiItems()
         tasks = Cubes2::mikuType("NxTask").select{|item| item["engine-0020"] }
+                .sort_by{|item| NxListings::dayCompletionRatio(item) }
+
         listings1 = Cubes2::mikuType("NxListing")
                     .select{|listing| listing["engine-0020"]["type"] == "daily-hours" }
+                    .sort_by{|item| NxListings::dayCompletionRatio(item) }
+
         listings2 = Cubes2::mikuType("NxListing")
                     .select{|listing| listing["engine-0020"]["type"] != "daily-hours" }
+                    .sort_by{|item| NxListings::dayCompletionRatio(item) }
+
         (tasks + listings1 + listings2)
             .select{|item| NxListings::dayCompletionRatio(item) < 1 }
-            .sort_by{|item| NxListings::dayCompletionRatio(item) }
     end
 end
