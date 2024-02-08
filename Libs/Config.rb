174a5126b46c6740dab62c79fa99d46f039853d6
Pascal Honoré <pascal@alseyn.net>
2024-02-08 21:33:53 +0000

    change primary instance

--- Libs/Config.rb
+++ Libs/Config.rb
@@ -29,7 +29,7 @@ class Config
 
     # Config::isPrimaryInstance()
     def self.isPrimaryInstance()
-        Config::thisInstanceId() == "Lucille20-pascal"
+        Config::thisInstanceId() == "Lucille18-pascal"
     end
 
     # Config::pathToCatalystDataRepository()
