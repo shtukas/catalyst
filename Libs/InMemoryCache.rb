0794c45a83dc4d787bb76e99fc88b57ebada3487
Pascal Honoré <pascal@alseyn.net>
2024-05-28 10:53:38 +0100

    datatrace caching

--- Libs/InMemoryCache.rb
+++ Libs/InMemoryCache.rb
@@ -7,24 +7,11 @@ class InMemoryCache
 
     # InMemoryCache::getOrNull(key)
     def self.getOrNull(key)
-        if $InMemoryX1k23[key] then
-            return $InMemoryX1k23[key].clone
-        end
-
-        value = XCache::getOrNull("3f74467c-e23d-49a9-bfab-6863264cdab8:#{key}")
-
-        if value then
-            value = JSON.parse(value)[0]
-            $InMemoryX1k23[key] = value
-            value
-        else
-            nil
-        end
+        $InMemoryX1k23[key]
     end
 
     # InMemoryCache::set(key, value)
     def self.set(key, value)
         $InMemoryX1k23[key] = value
-        XCache::set("3f74467c-e23d-49a9-bfab-6863264cdab8:#{key}", JSON.generate([value]))
     end
 end
