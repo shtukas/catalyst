794f9af8de222b24dc2fac2b2284087fa7e1cf56
Pascal Honoré <pascal@alseyn.net>
2025-02-25 16:52:43 +0000

    Timings update

--- Libs/Timings.rb
+++ Libs/Timings.rb
@@ -14,7 +14,7 @@ class Timings
     def self.lap(message)
         dt = Time.new.to_f - $B427FCD6
         filepath = "#{Config::userHomeDirectory()}/x-space/catalyst-timings.txt"
-        File.open(filepath, 'a'){|f| f.puts("#{message} -> #{dt}") }
+        File.open(filepath, 'a'){|f| f.puts("#{message} -> #{dt.round(3)}") }
         $B427FCD6 = Time.new.to_f
     end
 end
