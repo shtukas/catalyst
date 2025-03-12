eac40209ad8af254f41eb1bf2dd106c77ade1475
Pascal Honoré <pascal@alseyn.net>
2025-03-12 12:48:30 +0000

    rust: mikuType from database

--- rust/catalyst/src/items.rs
+++ rust/catalyst/src/items.rs
@@ -1,5 +1,5 @@
 use serde_json::{Value as JSONValue};
-use rusqlite::{Connection, Error};
+use rusqlite::{params, Connection, Error};
 
 //#[derive(Serialize, Deserialize)]
 #[derive(Debug)]
@@ -39,4 +39,32 @@ pub fn get_partial_items_1(conn: &Connection) -> Result<Vec<PartialItem>, Error>
 pub fn get_partial_items_2() -> Vec<PartialItem> {
     let conn = Connection::open("/Users/pascal/Galaxy/DataHub/Catalyst/data/Items/20240607-155704-609823.sqlite3").unwrap();
     get_partial_items_1(&conn).unwrap()
+}
+
+pub fn get_mikuType_partial_items_1(conn: &Connection, mikuType: String) -> Result<Vec<PartialItem>, Error> {
+    let mut stmt = conn.prepare("select _uuid_, _mikuType_, _item_ from Items where _mikuType_ = ?")?;
+    let elements = stmt.query_map(params![mikuType], |row| {
+        // Items (_uuid_ string primary key, _mikuType_ string, _item_ string)
+        let uuid: String = row.get(0).unwrap();
+        let mikuType = row.get(1).unwrap();
+        let item_as_json_string: String = row.get(2).unwrap();
+        Ok((uuid, mikuType, item_as_json_string))
+    })?;
+    let mut answer: Vec<PartialItem> = Vec::new();
+    for element in elements {
+        let tuple = element.unwrap();
+        answer.push(
+            PartialItem{
+                uuid: tuple.0,
+                mikuType: tuple.1,
+                json_value: string_to_json_value(&tuple.2)
+            }
+        );
+    }
+    Ok(answer)
+}
+
+pub fn get_mikuType_partial_items_2(mikuType: String) -> Vec<PartialItem> {
+    let conn = Connection::open("/Users/pascal/Galaxy/DataHub/Catalyst/data/Items/20240607-155704-609823.sqlite3").unwrap();
+    get_mikuType_partial_items_1(&conn, mikuType).unwrap()
 }
--- rust/catalyst/src/main.rs
+++ rust/catalyst/src/main.rs
@@ -6,25 +6,7 @@ use nxanniversaries::NxAnniversary;
 
 mod nxfloats;
 mod items;
-use items::{get_partial_items_1, get_partial_items_2, PartialItem};
-
-fn get_anniversaries_from_database() -> NxAnniversary {
-    return NxAnniversary {
-        uuid: "c117cc81-6c05-4dc2-9594-2d044b9e05a5".into(),
-        mikuType: "NxAnniversary".into(),
-        unixtime: 1741335180,
-        datetime: "2025-03-07T08:14:15Z".into(),
-        startdate: "2025-03-01".into(),
-        repeatType: "montly".into(),
-        next_celebration: "2025-04-01".into(),
-    };
-}
-
-fn print_listing1(listing: Vec<NxAnniversary>) {
-    listing.iter().for_each(|anniversary: &NxAnniversary| {
-        println!("Anniversary from the database: {:?}", anniversary)
-    });
-}
+use items::{get_mikuType_partial_items_2, get_partial_items_1, get_partial_items_2, PartialItem};
 
 fn print_listing2(items: Vec<PartialItem>) {
     items.iter().for_each(|item: &PartialItem| {
@@ -33,10 +15,7 @@ fn print_listing2(items: Vec<PartialItem>) {
 }
 
 fn main() {
-    let anniversary: NxAnniversary = get_anniversaries_from_database();
-    print_listing1(vec![anniversary]);
-
-
-    let items = get_partial_items_2();
+    let mikuType = String::from("NxFloat");
+    let items = get_mikuType_partial_items_2(mikuType);
     print_listing2(items);
 }
