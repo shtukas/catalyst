6d02578d623eaa940d04ce57192a768c7630c4c4
Pascal Honoré <pascal@alseyn.net>
2025-06-06 20:04:22 +0100

    ditpatch corrections

--- Libs/Dispatch.rb
+++ Libs/Dispatch.rb
@@ -27,18 +27,21 @@ class Dispatch
     # Dispatch::pickup()
     def self.pickup()
         directory = "#{Config::userHomeDirectory()}/Galaxy/DataHub/Catalyst/data/Dispatch/#{Config::thisInstanceId()}"
-        LucilleCore::locationsAtFolder(directory)
-            .select{|filepath| filepath[-5, 5] == ".json" }
-            .each{|filepath|
-                notification = JSON.parse(IO.read(filepath))
-                puts "notification: #{notification.to_s}".yellow
-                if notification[0] == "update" then
-                    HardProblem::blade_has_been_updated(notification[1])
-                end
-                if notification[0] == "destroy" then
-                    HardProblem::blade_has_been_destroyed(notification[1])
-                end
-                FileUtils.rm(filepath)
-            }
+        notifications = LucilleCore::locationsAtFolder(directory)
+                            .select{|filepath| filepath[-5, 5] == ".json" }
+                            .map{|filepath| JSON.parse(IO.read(filepath)) }
+        updateuuids = notifications
+                        .select{|notification| notification[0] == "update" }
+                        .map{|notification| notification[1] }
+                        .uniq # nice one Pascal!
+
+        destroyuuids = notifications
+                        .select{|notification| notification[0] == "destroy" }
+                        .map{|notification| notification[1] }
+                        .uniq # nice one Pascal!
+
+        updateuuids.each{|uuid| HardProblem::blade_has_been_updated(uuid) }
+
+        destroyuuids.each{|uuid| HardProblem::blade_has_been_destroyed(uuid) }
     end
 end
