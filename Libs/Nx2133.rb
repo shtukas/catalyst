9465e671d98244e6344269ed681f52d86ddf0d2a
Pascal Honoré <pascal@alseyn.net>
2025-07-04 11:48:49 +0100

    Nx2133::updates, Nx2133::optimiseNonDeadlinesPlacement2

--- Libs/Nx2133.rb
+++ Libs/Nx2133.rb
@@ -108,6 +108,7 @@ class Nx2133
         (0..items.size-1).each{|i|
             (i..items.size-1).each{|j|
                 if items[i]["nx2133"]["deadline"] and items[j]["nx2133"]["deadline"] and items[i]["nx2133"]["deadline"] > items[j]["nx2133"]["deadline"] then
+                    puts "ensure deadline ordering: permute: '#{PolyFunctions::toString(items[i]).green}'' and '#{PolyFunctions::toString(items[j]).green}'".yellow
                     items = Nx2133::permutePositions(items, i, j)
                 end
             }
@@ -119,6 +120,7 @@ class Nx2133
     def self.ensureDeadlineProjections1(items, indx, time)
         # Time represents the time at which we start the item at position indx
         if items[indx]["nx2133"]["deadline"] and items[indx]["nx2133"]["deadline"] < Time.at(time).utc.iso8601 and items[indx-1]["nx2133"]["deadline"].nil? then
+            puts "ensure deadline projections: permute: '#{PolyFunctions::toString(items[indx-1]).green}' and '#{PolyFunctions::toString(items[indx]).green}'".yellow
             items = Nx2133::permutePositions(items, indx-1, indx)
         end
         items
@@ -135,10 +137,32 @@ class Nx2133
         items
     end
 
+    # Nx2133::optimiseNonDeadlinesPlacement1(items, indx, time)
+    def self.optimiseNonDeadlinesPlacement1(items, indx, time)
+        # Time represents the time at which we start the item at position indx
+        if items[indx]["nx2133"]["deadline"] and items[indx+1]["nx2133"]["deadline"].nil? and items[indx]["nx2133"]["deadline"] > Time.at(time + items[indx]["nx2133"]["duration"]*60 + items[indx+1]["nx2133"]["duration"]*60).utc.iso8601 then
+            puts "optimise non deadline placement: permute: '#{PolyFunctions::toString(items[indx]).green}' and '#{PolyFunctions::toString(items[indx+1]).green}'".yellow
+            items = Nx2133::permutePositions(items, indx, indx+1)
+        end
+        items
+    end
+
+    # Nx2133::optimiseNonDeadlinesPlacement2(items)
+    def self.optimiseNonDeadlinesPlacement2(items)
+        return items if items.size < 2
+        time = Time.new.to_i + items[0]["nx2133"]["duration"] * 60
+        (1..items.size-1).each{|indx|
+            items = Nx2133::optimiseNonDeadlinesPlacement1(items, indx, time)
+            time = time + items[indx-1]["nx2133"]["duration"] * 60
+        }
+        items
+    end
+
     # Nx2133::updates(items)
     def self.updates(items)
         items = Nx2133::ensureDeadlineOrdering(items)
         items = Nx2133::ensureDeadlineProjections2(items)
+        items = Nx2133::optimiseNonDeadlinesPlacement2(items)
         items
     end
 
