d25072271f01470edb9ae835699f99d62781f69b
Pascal Honoré <pascal@alseyn.net>
2025-07-22 09:10:59 +0100

    NxTask to NxProject

--- Libs/PolyActions.rb
+++ Libs/PolyActions.rb
@@ -81,6 +81,15 @@ class PolyActions
     # PolyActions::stop(item)
     def self.stop(item)
         NxBalls::stop(item)
+        if item["mikuType"] == "NxTask" then
+            if LucilleCore::askQuestionAnswerAsBoolean("You are stopping a NxTask, should we transmute it to a NxProject") then
+                Items::setAttribute(item["uuid"], "project-position", NxProjects::getNextPosition())
+                Items::setAttribute(item["uuid"], "commitment-date", CommonUtils::today())
+                Items::setAttribute(item["uuid"], "commitment-hours", 0)
+                Items::setAttribute(item["uuid"], "mikuType", "NxProject")
+                item = Items::itemOrNull(item["uuid"])
+            end
+        end
         Index0::itemHasStoppedOrWasDoneOrWasDestroyed(item)
     end
 
