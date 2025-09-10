e3c4f252b93cb4aa5a0b373866fe238e452ad412
Pascal Honoré <pascal@alseyn.net>
2025-09-10 09:47:06 +0100

    payload for NxOpenInterest

--- Libs/NxOpenInterests.rb
+++ Libs/NxOpenInterests.rb
@@ -7,9 +7,11 @@ class NxOpenInterests
         description = LucilleCore::askQuestionAnswerAsString("description (empty to abort): ")
         return nil if description == ""
         Items::init(uuid)
+        payload = UxPayload::makeNewOrNull(uuid)
         Items::setAttribute(uuid, "unixtime", Time.new.to_i)
         Items::setAttribute(uuid, "datetime", Time.new.utc.iso8601)
         Items::setAttribute(uuid, "description", description)
+        Items::setAttribute(uuid, "uxpayload-b4e4", payload)
         Items::setAttribute(uuid, "mikuType", "NxOpenInterest")
         Items::itemOrNull(uuid)
     end
