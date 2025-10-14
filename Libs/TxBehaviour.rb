8eb7f4cb306fe06cc4dca5bc5b228eb36982392a
Pascal Honoré <pascal@alseyn.net>
2025-10-14 13:21:57 +0100

    introduce behaviour for awaits

--- Libs/TxBehaviour.rb
+++ Libs/TxBehaviour.rb
@@ -3,10 +3,38 @@ class TxBehaviour
 
     # TxBehaviour::behaviourToListingPosition(behavior)
     def self.behaviourToListingPosition(behavior)
-        if behavior["listing-position"] then
+        # There should not be negative positions
+
+        # 0.010 -> 0.020 Wave interruption
+        # 0.100 -> 0.150 NxEvents
+        # 0.160 -> 0.160 NxAwait
+        # 0.260 -> 0.280 NxAnniversary
+        # 0.280 -> 0.300 NxLambda
+        # 0.300 -> 0.320 Wave sticky
+        # 0.400 -> 0.450 NxBackup
+        # 0.500 -> 0.600 NxOnDate
+        # 0.750 -> 0.780 NxProject
+        # 0.800 -> 0.880 NxTask
+        # 0.390 -> 1.000 Wave (overlay)
+
+        # {
+        #     "btype": "listing-position"
+        #     "position": Float
+        # }
+        if behavior["btype"] == "listing-position" then
             return behavior["position"]
         end
-        return 0
+
+        #{
+        #    "btype": "NxAwait"
+        #    "creationUnixtime": Float
+        #}
+        if behavior["btype"] == "NxAwait" then
+            dx = ListingService::realLineTo01Increasing(behavior["creationUnixtime"] - 1759082216)
+            return 0.160 + dx.to_f/1000
+        end
+
+        raise "I do not know how to compute listing position for behaviour: #{behaviour}"
     end
 
     # TxBehaviour::doneBehaviour(behavior: TxBehaviour) -> TxBehaviour
@@ -18,6 +46,16 @@ class TxBehaviour
         if behavior["btype"] == "listing-position" then
             return nil # it's being destroyed
         end
+
+        #{
+        #    "btype": "NxAwait"
+        #    "creationUnixtime": Float
+        #}
+        if behavior["btype"] == "NxAwait" then
+            return nil # it's being destroyed
+        end
+
+        raise "I do not know how to perform done for behaviour: #{behaviour}"
     end
 
     # TxBehaviour::doneBehaviours(behaviours: Array[TxBehaviour]) -> Array[TxBehaviour]
