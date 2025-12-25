b9633e4385558afeff95aeb08be46c1b92bb6756
Pascal Honoré <pascal@alseyn.net>
2025-12-25 21:24:32 +0000

    Blades Return (part 1)

---
+++ Libs/Blades.rb
@@ -0,0 +1,50 @@
+
+# encoding: UTF-8
+
+=begin
+
+Blades are the file where we store catalyst items data
+
+create table version (version Integer primary key);
+create table attributes (key TEXT primary key, value TEXT); # values are always JSON encoded
+create table datablobs (nhash TEXT primary key, data BLOB);
+
+There must at least be 
+    - one attribute called uuid that is the unique identifier of the item
+    - one attribute called mikuType
+
+=end
+
+class Blades
+
+    # -------------------------------------
+    # Private
+
+    # Blades::repository_path()
+    def self.repository_path()
+        "#{Config::pathToCatalystDataRepository()}/blades"
+    end
+
+    # Blades::ensure_canonical_path(filepath)
+    def self.ensure_canonical_path(filepath)
+        return if !File.exist?(filepath)
+        canonical_filename = "#{Digest::SHA1.file(filepath).hexdigest}.blade"
+        canonical_filepath = "#{Blades::repository_path()}/#{canonical_filename[0, 2]}/#{canonical_filename}"
+        return if filepath == canonical_filepath
+        if !File.exist?(File.dirname(canonical_filepath)) then
+            FileUtils.mkdir(File.dirname(canonical_filepath))
+        end
+        FileUtils.mv(filepath, canonical_filepath)
+    end
+
+    # -------------------------------------
+    # Public interface
+
+    #Blades::init(uuid, mikuType)
+    def self.init(uuid, mikuType)
+        # create a new blade
+
+        filepath = "#{Blades::repository_path()}"
+
+    end
+end
